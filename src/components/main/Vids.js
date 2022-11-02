faf7a0866a0b877ddf8825020f618791bcbd2dc7
ChanghyeonO <ckdgus5189@gmail.com>
2022-11-02 23:54:55 +0900

    슬라이더 영상 추가

--- src/components/main/Vids.js
+++ src/components/main/Vids.js
@@ -1,34 +1,72 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 import { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import Popup from "../common/Popup";
 
 function Vids() {
-    return (
-        <main id="vids" className='myScroll'>
-            <Swiper
-                modules={[Pagination, Navigation]}
-                pagination={
-                    {
-                        clickable: true,
-                    }
-                }
-                spaceBetween={60}
-                navigation={true}
-                loop={true}
-                slidesPerView={3}
-                centeredSlides={true}
-            >
-                <SwiperSlide><div className="inner"></div></SwiperSlide>
-                <SwiperSlide><div className="inner"></div></SwiperSlide>
-                <SwiperSlide><div className="inner"></div></SwiperSlide>
-                <SwiperSlide><div className="inner"></div></SwiperSlide>
-                <SwiperSlide><div className="inner"></div></SwiperSlide>
-            </Swiper>
-            {/* <video src={process.env.PUBLIC_URL + '/img/sky.mp4'} loop autoPlay muted></video> */}
-        </main>
-    );
+  const pop = useRef(null);
+  const [Vids, setVids] = useState([]);
+  const [Index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const key = "AIzaSyAKqZ1Dx9awi1lCS84qziASeQYZJqLxLSM";
+    const playlist = "PLUcaufaEsTHA0MA_IcgsC_AQ86TpBOYOw";
+    const num = 6;
+    const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
+
+    axios.get(url).then((json) => {
+      setVids(json.data.items);
+    });
+  }, []);
+
+  return (
+    <main id="vids" className="myScroll">
+      <Swiper
+        modules={[Pagination, Navigation]}
+        pagination={{
+          clickable: true,
+        }}
+        spaceBetween={60}
+        navigation={true}
+        loop={true}
+        slidesPerView={3}
+        centeredSlides={true}
+      >
+        {Vids.map((data, index) => {
+          return (
+            <SwiperSlide>
+              <article key={index}>
+                <div
+                  key={index}
+                  className="inner"
+                  onClick={() => {
+                    pop.current.open();
+                    setIndex(index);
+                  }}
+                >
+                  <img
+                    src={data.snippet.thumbnails.standard.url}
+                    alt={data.snippet.title}
+                  />
+                </div>
+              </article>
+            </SwiperSlide>
+          );
+        })}
+        <Popup ref={pop}>
+          {Vids.length !== 0 && (
+            <iframe
+              src={`https://www.youtube.com/embed/${Vids[Index].snippet.resourceId.videoId}`}
+              frameBorder="0"
+            ></iframe>
+          )}
+        </Popup>
+      </Swiper>
+    </main>
+  );
 }
 export default Vids;
--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -10,7 +10,7 @@ export default function Youtube() {
 
     useEffect(() => {
         const key = 'AIzaSyAKqZ1Dx9awi1lCS84qziASeQYZJqLxLSM';
-        const playlist = "PLtt429gshWMp4G-VhNTFhBzBTd7GOEz-G";
+        const playlist = "PLUcaufaEsTHA0MA_IcgsC_AQ86TpBOYOw";
         const num = 6;
         const url = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${key}&playlistId=${playlist}&maxResults=${num}`;
 
